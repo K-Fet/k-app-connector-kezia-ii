de83378daa2f99d74dc7403728e75bde708bcbe4
Marc-Antoine FERNANDES <marcantoinefernandes@gmail.com>
2019-01-12 10:53:13 +0100

    Add coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,13 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
   // The test environment that will be used for testing
   testEnvironment: 'node',
 };
