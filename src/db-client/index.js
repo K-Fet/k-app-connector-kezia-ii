4687f2e1750f342c316b14a9ba1839ff399ddf6f
Marc-Antoine Fernandes <marcantoinefernandes@gmail.com>
2019-02-17 19:01:12 +0100

    Fix #2

--- src/db-client/index.js
+++ src/db-client/index.js
@@ -2,21 +2,31 @@ const genericPool = require('generic-pool');
 const { Database } = require('./odbc-promise');
 
 const factory = {
-  create: () => new Promise((resolve, reject) => {
+  create: async () => {
     const db = new Database();
-
-    db.open(process.env.ODBC_CN)
-      .then(() => resolve(db))
-      .catch(reject);
-  }),
-  destroy: (db) => {
-    db.close();
+    console.log('Creating new database instance');
+    await db.open(process.env.ODBC_CN);
+    console.log('New database instance created');
+    return db;
+  },
+  destroy: db => db.close(),
+  validate: async (db) => {
+    console.log('Testing if connection is ok');
+    try {
+      await db.query('SELECT * FROM Article');
+      console.log('DB Connection is ok');
+      return true;
+    } catch (e) {
+      console.error('DB Connection is failing', e);
+      return false;
+    }
   },
 };
 
 const opts = {
-  max: 10, // maximum size of the pool
-  min: 2, // minimum size of the pool
+  testOnBorrow: true,
+  max: 5, // maximum size of the pool
+  min: 1, // minimum size of the pool
 };
 
 const pool = genericPool.createPool(factory, opts);
